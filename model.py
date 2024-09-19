add200815fe140ab67acc7458989d8d7752d6d1e
Sonia Palit <166869989+soniapalit@users.noreply.github.com>
2024-09-19 15:11:12 -0400

    did grid object, grid tick, changed class hierchy

--- model.py
+++ model.py
@@ -1,12 +1,12 @@
 #make snake object
 #move circle based on coordinates
 
-import pygame
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
+import pygame;
+pygame.init();
+screen = pygame.display.set_mode((1280, 720));
+clock = pygame.time.Clock();
+running = True;
+dt = 0;
 numRows, numCols = 15,15;
 
 
@@ -16,7 +16,7 @@ class Grid:
         self.numCols = c;
         self.cellWidth = screen.get_width()/r;
         self.cellLength = screen.get_height()/c;
-        self.snakeHead = SnakeSegment(self.getLocation(self.numRows//2),self.getLocation(self.numCols//2));
+        self.snakeHead = SnakeSegment(self.getLocationX(self.numRows//2),self.getLocationY(self.numCols//2));
 
     def getLocationY(self,row):
         return (self.cellLength*row) + (self.cellLength//2);
@@ -24,62 +24,56 @@ class Grid:
     def getLocationX (self, col):
         return (self.cellWidth*col) + (self.cellWidth//2);
 
+    def changeDirection (memyselfandi, dir):
+        memyselfandi.snakeHead.direction = dir;
+
+    def tick(self):
+        self.snakeHead.moveDir();
+
+
 
 
 class SnakeSegment:
     def __init__(self, x, y):
-        self.snake_pos = pygame.Vector2(x,y)
-        self.direction = "y+"
+        self.snake_pos = pygame.Vector2(x,y);
+        self.direction = "y+";
 
-    def moveDir(self):
-        dir = self.direction
-        speed=.5
+    def moveDir(self,speed=.5):
+        dir = self.direction;
         if dir=="y+":
-            self.snake_pos.y += speed
+            self.snake_pos.y += speed;
         elif dir =="y-":
-            self.snake_pos.y-=speed
+            self.snake_pos.y-=speed;
         elif dir =="x+":
-            self.snake_pos.x+=speed
+            self.snake_pos.x+=speed;
         else:
-            self.snake_pos.x-=speed
-
-    def moveDir(self,speed):
-        dir = self.direction
-        if dir=="y+":
-            self.snake_pos.y += speed
-        elif dir =="y-":
-            self.snake_pos.y-=speed
-        elif dir =="x+":
-            self.snake_pos.x+=speed
-        else:
-            self.snake_pos.x-=speed
+            self.snake_pos.x-=speed;
 
 
 snakeGrid = Grid(10,10);
-s = SnakeSegment();
 
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            running = False;
     screen.fill("blue")
-    a = pygame.Rect(s.snake_pos, (250, 50))
-    pygame.draw.ellipse(screen, "pink", a) 
+    a = pygame.Rect(snakeGrid.snakeHead.snake_pos, (250, 50));
+    pygame.draw.ellipse(screen, "pink", a) ;
 
     keys = pygame.key.get_pressed()
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        s.direction="y-"
+        snakeGrid.changeDirection("y-");
     if keys[pygame.K_s]:
-        s.direction = "y+"
+        snakeGrid.changeDirection ("y+");
     if keys[pygame.K_a]:
-        s.direction = "x-"
+        snakeGrid.changeDirection("x-");
     if keys[pygame.K_d]:
-        s.direction = "x+"
+        snakeGrid.changeDirection ("x+");
 
-    s.moveDir(1) #move
+    snakeGrid.tick() #move
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000
